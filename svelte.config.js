cf58b7fb1731bc6a486eb8a00bde15d50f63f979
griffin104 <griffinbrlw@gmail.com>
2022-07-13 17:10:37 -0400

    Updated svelte.config.js to not add extra files to exports

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,9 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+    package: {
+      exports: (filepath) => !/./.test(filepath)
+    },
 		adapter: adapter()
 	}
 };
