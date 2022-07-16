49786433d7860a24bf565ea90ba888b6e2363e68
Ilija Bibic <ibibic2@gmail.com>
2022-07-16 16:08:43 -0700

    Setup required validator logic in required.js file
    Co-authored-by: Griffin Barlow <griffin104@users.noreply.github.com>
    Co-authored-by: Steven Yuan <steven-yuann@users.noreply.github.com>
    Co-authored-by: Angel Cortes <angelcortes@tutanota.com>

---
+++ src/lib/validators/required.js
@@ -0,0 +1,18 @@
+import { formStore } from '$lib/store.js';
+
+export function required(value) {
+  let newStore
+  formStore.subscribe(store => {
+    newStore = store; 
+  })
+  if (!newStore.values[value]) {
+    if (!newStore.errors[value]) {
+      newStore.errors[value] = {
+        required: `Error. ${value} is a required field.`
+      } 
+    } else {
+      newStore.errors[value].required = `Error. ${value} is a required field.`
+    }
+    formStore.set(newStore)    
+  }
+}
