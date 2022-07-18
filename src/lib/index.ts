1d9dd061a4cc5e750782764ba338e139a7204c5e
Steven Yuan <stevenyuan@Stevens-MacBook-Pro.local>
2022-07-18 11:50:01 -1000

    SSR bug when importing FIXED

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,6 @@
-export { Button } from './Button/Button.svelte';
+//Import all rendering components
+import Button from './Button/Button.svelte'
+
+
+//Export all rendering components
+export { Button }; 
