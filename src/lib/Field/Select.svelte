9524d4087cd5baacf55c1f0f3e9d76d93aa0e42a
Ilija Bibic <ibibic2@gmail.com>
2022-07-19 16:05:13 -0700

    Select component created in Select.svelte

---
+++ src/lib/Field/Select.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { formStore } from '../store'
+    export let values = [];
+    export let name;
+     
+</script>
+
+<select bind:value={$formStore.values[name]} name={name}>
+ {#each values as value, i}
+ 
+ <option value={value}>
+    {value}
+</option>
+{/each}
+</select>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,7 @@
   //@ts-ignore
   import FastForm from "$lib/FastForm/FastForm.svelte"
   import Field from '$lib/Field/Field.svelte'
+  import Select from '$lib/Field/Select.svelte'
   // import Field from '../lib/Field/Field.svelte'
   let initValues = {
     icecream: []
@@ -14,5 +15,6 @@
 
 <FastForm initValues={initValues} handleSubmit={handleSubmit}>
   <Field name='icecream' type='checkbox' values={['1', '2', '3']}/>
+  <Select name='' values={['1', '2', '3']} />
   <button type='submit'>Submit!</button>
 </FastForm>
