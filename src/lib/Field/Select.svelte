731d02add4b16bfa2dbfb6c6ba9fcf2edc0b107d
Steven Yuan <stevenyuan@Stevens-MacBook-Pro.local>
2022-07-22 07:52:20 -1000

    updating to TS, add on:change and on:blur method

--- src/lib/Field/Select.svelte
+++ src/lib/Field/Select.svelte
@@ -1,11 +1,13 @@
-<script>
+<script lang="ts">
     import { formStore } from '../store'
-    export let values = [];
-    export let name;
-     
+    export let values:[];
+    export let name:string;
+    export let handleBlur:()=>void;
+    export let handleOnChange:()=>void; 
 </script>
 
-<select bind:value={$formStore.values[name]} name={name}>
+
+<select bind:value={$formStore.values[name]} name={name} on:blur={handleBlur} on:change={handleOnChange}>
  {#each values as value, i}
  
  <option value={value}>
