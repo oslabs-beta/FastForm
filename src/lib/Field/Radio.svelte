adb891f8a19c49fe1a2bda3ac31e5a04a92465be
Steven Yuan <stevenyuan@Stevens-MacBook-Pro.local>
2022-07-23 07:57:49 -1000

    option for onblur and onchange, added to all 3 components

--- src/lib/Field/Radio.svelte
+++ src/lib/Field/Radio.svelte
@@ -4,10 +4,12 @@
   export let type:string;
   export let values:string[];
   export let name:string;
+  export let handleBlur:()=>void;
+  export let handleOnChange:()=>void;
 
 </script>
 
 {#each values as value }
-  <input type="radio" name={name} value={value} bind:group={$formStore.values[name]}>
+  <input type="radio" name={name} value={value} bind:group={$formStore.values[name]} on:blur={handleBlur} on:input={handleOnChange}>
   <label for={value}>{value}</label>
 {/each}
