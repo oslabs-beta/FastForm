6ad2d51cc9a02b6b0ab0814610a3c72cd6e6134a
Steven Yuan <stevenyuan@Stevens-MacBook-Pro.local>
2022-12-08 12:34:56 -0800

    add isDate validator

--- src/lib/validators/isNumber/isNumber.ts
+++ src/lib/validators/isNumber/isNumber.ts
@@ -2,7 +2,7 @@ import type { formStoreValueType } from '../../types'
 
 export default function isNumber(field : string, fieldToMatch : string, store: formStoreValueType):formStoreValueType {
   if (!isNaN(Number(store.values[field]))) {
-    store.errors[field]['isNumber'] = `Error: Not a number`
+    store.errors[field]['isNumber'] = `Error: invalid number input`
   } 
   return store
 }
