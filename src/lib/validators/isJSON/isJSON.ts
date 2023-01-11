7fd0adae70707ad77141a8670db8d1cb8a4be7cf
Steven Yuan <stevenyuan@Stevens-MacBook-Pro.local>
2023-01-10 19:34:56 -0800

    json validator implementation

--- src/lib/validators/isJSON/isJSON.ts
+++ src/lib/validators/isJSON/isJSON.ts
@@ -1,8 +1,10 @@
 import type { formStoreValueType } from '../../types'
 
 export default function isJSON(field : string, store: formStoreValueType):formStoreValueType {
-  if (!isNaN(Number(store.values[field]))) {
-    store.errors[field]['isNumber'] = `Error: invalid number input`
-  } 
+  try {
+    JSON.parse(field)
+  } catch (e) {
+    store.errors[field]['isJSON'] = `Error: Not a valid JSON format`
+  }
   return store
 }
